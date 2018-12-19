cbffa2987565540f52d052c536a4ea6e8a5561d3
Aga <agnieszka.fatla.1997@gmail.com>
2018-12-19 20:40:13 +0100

    test

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from IPython.display import SVG
 WIDTH = 512
 HEIGHT = 512
 
-board = chess.Board("8/8/8/8/4N3/8/8/8 w - - 0 1")
-squares = board.attacks(chess.E4)
-a = SVG(chess.svg.board(board=board, squares=squares))
+board = chess.Board()
+'''squares = board.attacks(chess.E4)'''
+a = SVG(chess.svg.board(board=board))
 
 screen = pygame.display.set_mode([500, 500])
 
-
 print(board)
+
 
 
